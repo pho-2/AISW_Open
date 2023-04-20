948e8a9a860d66b4b29e89170097ff0d9912cfdc
seungho <k355323@gmail.com>
2023-04-20 20:28:45 +0900

    sec

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,9 @@ class MyApp extends StatelessWidget {
           Icon(Icons.star),
           Icon(Icons.star),
           Icon(Icons.star),
+          Icon(Icons.star),
+          Icon(Icons.star),
+          Icon(Icons.star),
         ],
       ),
     )
